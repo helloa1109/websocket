746917556c7bf32d85ed13c1092c3eba32a3e5df
helloa1109 <helloa1109@naver.com>
2023-07-03 20:12:26 +0900

    controller

---
+++ demo/src/main/java/com/example/demo/controller/lobbyController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.services.roomService;
+import com.example.demo.dto.roomDto;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/lobby")
+@Slf4j
+public class lobbyController {
+
+    @Autowired
+    roomService roomService;
+
+    @GetMapping("/list")
+    public List<roomDto> getList(){
+        // log.info("방목록 호출");
+        return roomService.getAll();
+    }
+
+    @PostMapping("/create")
+    public roomDto postCreate(@RequestBody Map<String, Object> data){
+        return roomService.createRoom(data.get("name").toString());
+    }
+}
